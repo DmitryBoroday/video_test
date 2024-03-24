396372a21e535847718ab21f50c644df91a4f719
Dmitry Boroday <dboroday2018@gmail.com>
2024-03-24 03:13:19 +0200

    second-commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 export default function Home() {
   return (
     <main>
-      <video
+      {/* <video
         className='w-full h-full object-cover overflow-hidden'
         loop
         muted
         autoPlay
-        src={require('/public/videos/dubai_banner.webm')} />
+        src={require('/public/videos/dubai_banner.webm')} /> */}
+      <video className='w-full' loop autoPlay muted>
+        <source src='/videos/colors_desktop.mp4'/>
+      </video>
     </main>
   );
 }
